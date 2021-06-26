83e42cd1dd7e95fa986c365fbfd3cc46253ac39c
Max Roncace <me@caseif.net>
2021-06-26 13:20:45 -0400

    Improve function loading, initial work on header generation

--- aglet.rb
+++ aglet.rb
@@ -4,7 +4,7 @@ require 'fileutils'
 require 'nokogiri'
 require 'optparse'
 
-GL_REGISTRY_PATH = "#{__dir__}/specs/EGL-Registry/xml/gl.xml"
+GL_REGISTRY_PATH = "#{__dir__}/specs/OpenGL-Registry/xml/gl.xml"
 
 GL_PROC_TYPE = "GLFWglproc"
 GL_LOOKUP_FN = "glfwGetProcAddress"
@@ -17,26 +17,28 @@ H_HEADER =
 
 #pragma once
 
-#include "GL/gl.h"
-#define GL_GLEXT_PROTOTYPES
-#include "GL/glext.h"
-
-#define GLFW_INCLUDE_NONE
-
-#include <stdbool.h>
-
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-bool agletLoad(void);
+#ifdef GL_GLEXT_VERSION
+#error "glext.h must not be included alongside aglet.h"
+#endif
+
+#define GLFW_INCLUDE_NONE
+
+#include <stdbool.h>
+'
+
+H_FOOTER =
+'bool agletLoad(void);
 
 #ifdef __cplusplus
 }
 #endif
 '
 
-TRAMPOLINES_HEADER =
+TRAMPOLINES_HEADER_AMD64 =
 ASM_COMMENT_PREFIX + 'Auto-generated file; do not modify!
 
 .intel_syntax noprefix
@@ -55,12 +57,7 @@ TRAMPLINE_TEMPLATE_AMD64 =
 LOADER_TEMPLATE =
 "/* Auto-generated file; do not modify! */
 
-#include <GL/gl.h>
-#define GL_GLEXT_PROTOTYPES
-#include <GL/glext.h>
-
-#define GLFW_INCLUDE_NONE
-#include \"GLFW/glfw3.h\"
+#include <aglet/aglet.h>
 
 #include <stdbool.h>
 
@@ -73,7 +70,7 @@ extern \"C\" {
 bool agletLoad() {
 %s
 
-return true;
+    return true;
 }
 
 #ifdef __cplusplus
@@ -89,7 +86,7 @@ class FnParam
     attr_reader :name
     attr_reader :type
 
-    def to_s()
+    def gen_c()
         return "#{@type} #{@name}"
     end
 end
@@ -104,10 +101,49 @@ class GLFunction
     attr_reader :ret_type
     attr_reader :params
 
-    def to_s()
+    def gen_c()
         fn_def = "#{@ret_type} #{@name}(#{params.join ', '})"
         return fn_def
     end
+end
+
+class GLType
+    def initialize(name, typedef)
+        @name = name
+        @typedef = typedef
+    end
+    attr_reader :name
+    attr_reader :typedef
+
+    def gen_c()
+        return "#{typedef}"
+    end
+end
+
+class GLEnum
+    def initialize(name, group, value)
+        @name = name
+        @group = group
+        @value = value
+    end
+    attr_reader :name
+    attr_reader :group
+    attr_reader :value
+
+    def gen_c()
+        return "#define #{name} #{value}"
+    end
+end
+
+class GLDefs
+    def initialize(types, enums, fns)
+        @types = types
+        @enums = enums
+        @fns = fns
+    end
+    attr_reader :types
+    attr_reader :enums
+    attr_reader :fns
 end
 
 def params_to_str(params)
@@ -122,24 +158,21 @@ def parse_args()
         opts.on('-p PROFILE', '--profile=PROFILE', 'Path to profile file') do |p|
             options[:profile] = p
         end
-        opts.on('-h HEADER OUTPUT', '--header-out=HEADER OUTPUT', 'Path to header output directory') do |h|
-            options[:header_output] = h
-        end
-        opts.on('-s SOURCE OUTPUT', '--source-out=SOURCE OUTPUT', 'Path to source output directory') do |s|
-            options[:source_output] = s
+        opts.on('-o OUTPUT', '--output=OUTPUT', 'Path to output directory') do |h|
+            options[:output] = h
         end
     end.parse!
 
     raise "Profile path is required" unless options[:profile]
-    raise "Header output path is required" unless options[:header_output]
-    raise "Source output path is required" unless options[:source_output]
+    raise "Output path is required" unless options[:output]
 
     options
 end
 
 def get_requested_fn_names(reg, profile_path)
+    require_types = []
+    require_enums = []
     require_fns = []
-    remove_fns = []
 
     profile = File.open(profile_path) { |f| Nokogiri::XML(f) }
     profile_api = profile.xpath('//profile/api/text()').text
@@ -154,8 +187,21 @@ def get_requested_fn_names(reg, profile_path)
         feature_major, feature_minor = feature_number.split('.')
         next if feature_major > req_major or (feature_major == req_major and feature_minor > req_minor)
 
-        require_fns.concat ver.xpath('.//require//command/@name').map { |c| c.text }
-        remove_fns.concat ver.xpath('.//remove[@profile="core"]//command/@name').map { |c| c.text } if profile_core
+        if profile_core
+            require_secs = ver.css("require:not([profile]), require[profile='core']")
+            remove_secs = ver.css("remove:not([profile]), remove[profile='core']")
+        else
+            require_secs = ver.css("require:not([profile]), require[profile='compatibility']")
+            remove_secs = ver.css("remove:not([profile]), remove[profile='compatibility']")
+        end
+
+        require_types += require_secs.xpath('.//type/@name').map { |n| n.text }
+        require_enums += require_secs.xpath('.//enum/@name').map { |n| n.text }
+        require_fns += require_secs.xpath('.//command/@name').map { |n| n.text }
+
+        require_types -= remove_secs.xpath('.//type/@name').map { |n| n.text }
+        require_enums -= remove_secs.xpath('.//enum/@name').map { |n| n.text }
+        require_fns -= remove_secs.xpath('.//command/@name').map { |n| n.text }
     end
 
     profile_extensions = profile.xpath('//profile//extensions//extension/text()').map { |e| e.text }
@@ -170,24 +216,24 @@ def get_requested_fn_names(reg, profile_path)
         ext_name = ext.xpath('@name').text
         next unless profile_extensions.include? ext_name
 
-        require_fns.concat ext.xpath('.//require//command/@name').map { |n| n.text }
-        remove_fns.concat ext.xpath('.//remove//command/@name').map { |n| n.text }
+        require_fns += ext.xpath('.//require//command/@name').map { |n| n.text }
+        require_fns -= ext.xpath('.//remove//command/@name').map { |n| n.text }
     end
 
-    final_fns = require_fns - remove_fns
-
     fmt_version = profile_version + (profile_core ? ' (core)' : '')
-    print "Found #{final_fns.length} functions for profile \"#{profile_api} #{fmt_version}\"\n"
+    print "Found #{require_fns.length} functions for profile \"#{profile_api} #{fmt_version}\"\n"
 
-    return final_fns
+    return require_fns
 end
 
 def parse_param_type(raw)
     raw.gsub(/<name>.*<\/name>/, '').gsub(/<\/?ptype>/, '')
 end
 
-def load_fn_defs(reg, req_fns)
+def load_gl_members(reg, profile_path)
     fns = []
+
+    req_fns = get_requested_fn_names(reg, profile_path)
 
     reg.xpath("//registry//commands//command").each do |cmd_root|
         cmd_name = cmd_root.xpath('.//proto//name')
@@ -217,6 +263,7 @@ def generate_header(out_dir, fns)
     out_file = File.open("#{out_dir}/aglet.h", 'w')
 
     out_file << H_HEADER
+    out_file << H_FOOTER
 end
 
 def generate_loader_source(out_dir, fns)
@@ -225,7 +272,7 @@ def generate_loader_source(out_dir, fns)
     load_code = ''
 
     fns.each_with_index do |fn, i|
-        load_code << "\n        #{ADDR_ARR}[#{i}] = #{GL_LOOKUP_FN}(\"#{fn.name}\");"
+        load_code << "    #{ADDR_ARR}[#{i}] = #{GL_LOOKUP_FN}(\"#{fn.name}\");\n"
     end
 
     load_code.delete_suffix! "\n"
@@ -233,10 +280,10 @@ def generate_loader_source(out_dir, fns)
     out_file << LOADER_TEMPLATE % [fns.size, load_code]
 end
 
-def generate_trampolines(out_dir, fns)
+def generate_trampolines_amd64(out_dir, fns)
     out_file = File.open("#{out_dir}/aglet_trampolines.s", 'w')
 
-    out_file << TRAMPOLINES_HEADER
+    out_file << TRAMPOLINES_HEADER_AMD64
 
     fns.each_with_index do |fn, i|
         out_file << "\n"
@@ -248,19 +295,15 @@ args = parse_args
 
 reg = File.open(GL_REGISTRY_PATH) { |f| Nokogiri::XML(f) }
 
-req_fns = get_requested_fn_names(reg, args[:profile])
-fn_defs = load_fn_defs(reg, req_fns)
+fn_defs = load_gl_members(reg, args[:profile])
 
-header_out_path = args[:header_output]
-source_out_path = args[:source_output]
+out_path = args[:output]
+header_out_path = "#{out_path}/include/aglet"
+source_out_path = "#{out_path}/src"
 
 FileUtils.mkdir_p header_out_path
 FileUtils.mkdir_p source_out_path
 
 generate_header(header_out_path, fn_defs)
 generate_loader_source(source_out_path, fn_defs)
-generate_trampolines(source_out_path, fn_defs)
-
-# copy glext.h, ideally we should generate this in-house eventually
-FileUtils.mkdir_p "#{header_out_path}/GL"
-FileUtils.cp("#{__dir__}/specs/EGL-Registry/api/GL/glext.h", "#{header_out_path}/GL/glext.h")
+generate_trampolines_amd64(source_out_path, fn_defs)
