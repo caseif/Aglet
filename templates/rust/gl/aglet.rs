5b2765c246df7a83dfe4a174f80e9793c9bd7890
Max Roncace <me@caseif.net>
2026-07-02 17:31:15 -0400

    Fix compile warnings in 2024 edition

--- templates/rust/gl/aglet.rs
+++ templates/rust/gl/aglet.rs
@@ -68,13 +68,13 @@ pub type GLuint64 = u64;
 pub type GLuint64EXT = u64;
 #[allow(dead_code, non_camel_case_types)]
 pub type GLsync = *mut std::ffi::c_void;
-#[allow(dead_code, non_camel_case_types)]
+#[allow(dead_code, non_camel_case_types, non_snake_case)]
 pub type GLDEBUGPROC = unsafe extern "C" fn(source: GLenum, ty: GLenum, id: GLuint, severity: GLenum, length: GLsizei, message: *const GLchar, userParam: *const std::ffi::c_void);
 #[allow(dead_code, non_camel_case_types)]
 pub type GLDEBUGPROCARB = GLDEBUGPROC;
 #[allow(dead_code, non_camel_case_types)]
 pub type GLDEBUGPROCKHR = GLDEBUGPROC;
-#[allow(dead_code, non_camel_case_types)]
+#[allow(dead_code, non_camel_case_types, non_snake_case)]
 pub type GLDEBUGPROCAMD = unsafe extern "C" fn(id: GLuint, category: GLenum, severity: GLenum, length: GLsizei, message: *const GLchar, userParam: *mut std::ffi::c_void);
 #[allow(dead_code, non_camel_case_types)]
 pub type GLhalfNV = u16;
@@ -92,7 +92,7 @@ pub enum AgletError {
     MissingExtension = 5,
 }
 
-type AgletLoadProc = unsafe extern "C" fn(name: *const std::ffi::c_char) -> *mut std::ffi::c_void;
+type AgletLoadProc = fn(name: &str) -> *mut std::ffi::c_void;
 
 static mut DID_LOAD_CAPS: bool = false;
 
@@ -111,7 +111,7 @@ pub const @{name}: u@{width} = @{value};
 #= /foreach =#
 
 #= foreach proc_defs =#
-#[allow(non_camel_case_types)]
+#[allow(non_camel_case_types, non_snake_case)]
 pub type PFN@{name_upper}PROC = unsafe extern "C" fn(@{params}) -> @{ret_type};
 #= /foreach =#
 
@@ -140,39 +140,43 @@ pub fn @{name}(@{params}) -> @{ret_type} { unsafe { (aglet_@{name}.unwrap())(@{p
 
 #[allow(dead_code, non_snake_case)]
 unsafe fn load_versions(load_proc: AgletLoadProc) -> Result<(), AgletError> {
-    let pn_glGetError = std::ffi::CString::new("glGetError").unwrap();
-    let local_glGetError_ptr = load_proc(pn_glGetError.as_ptr());
+    let local_glGetError_ptr = load_proc("glGetError");
     if local_glGetError_ptr.is_null() {
         return Err(AgletError::ProcLoad);
     }
-    let local_glGetError: unsafe extern "C" fn() -> GLenum = std::mem::transmute(local_glGetError_ptr);
+    let local_glGetError: unsafe extern "C" fn() -> GLenum = unsafe {
+        std::mem::transmute(local_glGetError_ptr)
+    };
 
     #= if @{target_api_version_major} >= 3 =#
-    let pn_glGetIntegerv = std::ffi::CString::new("glGetIntegerv").unwrap();
-    let local_glGetIntegerv_ptr = load_proc(pn_glGetIntegerv.as_ptr());
+    let local_glGetIntegerv_ptr = load_proc("glGetIntegerv");
     if local_glGetIntegerv_ptr.is_null() {
         return Err(AgletError::ProcLoad);
     }
-    let local_glGetIntegerv: unsafe extern "C" fn(GLenum, *mut GLint) = std::mem::transmute(local_glGetIntegerv_ptr);
+    let local_glGetIntegerv: unsafe extern "C" fn(GLenum, *mut GLint) = unsafe {
+        std::mem::transmute(local_glGetIntegerv_ptr)
+    };
 
-    let gl_err = local_glGetError();
+    let gl_err = unsafe { local_glGetError() };
     if gl_err == GL_NO_ERROR {
         let mut ver_major: i32 = 0;
         let mut ver_minor: i32 = 0;
 
-        local_glGetIntegerv(GL_MAJOR_VERSION, &mut ver_major);
-        if local_glGetError() != GL_NO_ERROR {
+        unsafe { local_glGetIntegerv(GL_MAJOR_VERSION, &mut ver_major); }
+        if unsafe { local_glGetError() } != GL_NO_ERROR {
             return Err(AgletError::GlError);
         }
 
-        local_glGetIntegerv(GL_MINOR_VERSION, &mut ver_minor);
-        if local_glGetError() != GL_NO_ERROR {
+        unsafe { local_glGetIntegerv(GL_MINOR_VERSION, &mut ver_minor); }
+        if unsafe { local_glGetError() } != GL_NO_ERROR {
             return Err(AgletError::GlError);
         }
 
-        #= foreach api_versions =#
-        AGLET_@{name} = ver_major > @{major} || (ver_major == @{major} && ver_minor >= @{minor});
-        #= /foreach =#
+        unsafe {
+            #= foreach api_versions =#
+            AGLET_@{name} = ver_major > @{major} || (ver_major == @{major} && ver_minor >= @{minor});
+            #= /foreach =#
+        }
 
         return Ok(());
     } else if gl_err != GL_INVALID_ENUM {
@@ -182,19 +186,21 @@ unsafe fn load_versions(load_proc: AgletLoadProc) -> Result<(), AgletError> {
 
     // fallback section
 
-    let pn_glGetString = std::ffi::CString::new("glGetString").unwrap();
-    let local_glGetString_ptr = load_proc(pn_glGetString.as_ptr());
+    let local_glGetString_ptr = load_proc("glGetString");
     if local_glGetString_ptr.is_null() {
         return Err(AgletError::ProcLoad);
     }
-    let local_glGetString: unsafe extern "C" fn(GLenum) -> *const GLubyte = std::mem::transmute(local_glGetString_ptr);
+    let local_glGetString: unsafe extern "C" fn(GLenum) -> *const GLubyte = unsafe {
+        std::mem::transmute(local_glGetString_ptr)
+    };
 
-    let ver_c_str: *const std::ffi::c_uchar = local_glGetString(GL_VERSION);
-    if local_glGetError() != GL_NO_ERROR || ver_c_str.is_null() {
-        return Err(AgletError::GlError);
-    }
-
-    let ver_str: std::string::String = std::ffi::CStr::from_ptr(ver_c_str.cast()).to_string_lossy().to_string();
+    let ver_str: std::string::String = unsafe {
+        let ver_c_str: *const std::ffi::c_uchar = local_glGetString(GL_VERSION);
+        if local_glGetError() != GL_NO_ERROR || ver_c_str.is_null() {
+            return Err(AgletError::GlError);
+        }
+        std::ffi::CStr::from_ptr(ver_c_str.cast()).to_string_lossy().to_string()
+    };
     if !ver_str.as_str().contains(".") {
         eprintln!("[Aglet] GL returned nonsense version string {ver_str}");
         return Err(AgletError::Unspecified);
@@ -221,52 +227,61 @@ unsafe fn load_versions(load_proc: AgletLoadProc) -> Result<(), AgletError> {
         Err(_) => return Err(AgletError::Unspecified),
     };
 
-    #= foreach api_versions =#
-    if parsed_major > @{major} || (parsed_major == @{major} && parsed_minor >= @{minor}) {
-        AGLET_@{name} = true;
+    unsafe {
+        #= foreach api_versions =#
+        if parsed_major > @{major} || (parsed_major == @{major} && parsed_minor >= @{minor}) {
+            AGLET_@{name} = true;
+        }
+        #= /foreach =#
     }
-    #= /foreach =#
 
     return Ok(());
 }
 
 #[allow(dead_code, non_snake_case)]
 unsafe fn load_extensions(load_proc: AgletLoadProc) -> Result<(), AgletError> {
-    let pn_glGetError = std::ffi::CString::new("glGetError").unwrap();
-    let local_glGetError_ptr = load_proc(pn_glGetError.as_ptr());
+    let local_glGetError_ptr = load_proc("glGetError");
     if local_glGetError_ptr.is_null() {
         return Err(AgletError::ProcLoad);
     }
-    let local_glGetError: unsafe extern "C" fn() -> GLenum = std::mem::transmute(local_glGetError_ptr);
+    let local_glGetError: unsafe extern "C" fn() -> GLenum = unsafe {
+        std::mem::transmute(local_glGetError_ptr)
+    };
 
     #= if @{target_api_version_major} >= 3 =#
-    let pn_glGetStringi = std::ffi::CString::new("glGetStringi").unwrap();
-    let pn_glGetIntegerv = std::ffi::CString::new("glGetIntegerv").unwrap();
-    let local_glGetStringi_ptr = load_proc(pn_glGetStringi.as_ptr());
-    let local_glGetIntegerv_ptr = load_proc(pn_glGetIntegerv.as_ptr());
+    let local_glGetStringi_ptr = load_proc("glGetStringi");
+    let local_glGetIntegerv_ptr = load_proc("glGetIntegerv");
     if !local_glGetStringi_ptr.is_null() && !local_glGetIntegerv_ptr.is_null() {
-        let local_glGetStringi: unsafe extern "C" fn(GLenum, GLuint) -> *const GLubyte = std::mem::transmute(local_glGetStringi_ptr);
-        let local_glGetIntegerv: unsafe extern "C" fn(GLenum, *mut GLint) = std::mem::transmute(local_glGetIntegerv_ptr);
+        let local_glGetStringi: unsafe extern "C" fn(GLenum, GLuint) -> *const GLubyte = unsafe {
+            std::mem::transmute(local_glGetStringi_ptr)
+        };
+        let local_glGetIntegerv: unsafe extern "C" fn(GLenum, *mut GLint) = unsafe {
+            std::mem::transmute(local_glGetIntegerv_ptr)
+        };
 
         let mut num_exts: i32 = 0;
-        local_glGetIntegerv(GL_NUM_EXTENSIONS, &mut num_exts);
+        unsafe { local_glGetIntegerv(GL_NUM_EXTENSIONS, &mut num_exts); }
 
         if num_exts < 0 {
             // this should never happen, something has gone catastrophically wrong
             return Err(AgletError::Unspecified);
         }
 
-        let gl_err = local_glGetError();
+        let gl_err = unsafe { local_glGetError() };
         if gl_err == GL_NO_ERROR {
             for i in 0u32..(num_exts as u32) {
-                let cur_ext: std::string::String = std::ffi::CStr::from_ptr(local_glGetStringi(GL_EXTENSIONS, i).cast()).to_string_lossy().to_string();
+                let cur_ext: std::string::String = unsafe {
+                    std::ffi::CStr::from_ptr(local_glGetStringi(GL_EXTENSIONS, i).cast()).to_string_lossy().to_string()
+                };
 
-                #= foreach extensions =#
-                if cur_ext == "@{name}" {
-                    AGLET_@{name} = true;
-                    continue;
+                unsafe {
+                    #= foreach extensions =#
+                    if cur_ext == "@{name}" {
+                        AGLET_@{name} = true;
+                        continue;
+                    }
+                    #= /foreach =#
                 }
-                #= /foreach =#
             }
 
             return Ok(());
@@ -278,16 +293,19 @@ unsafe fn load_extensions(load_proc: AgletLoadProc) -> Result<(), AgletError> {
 
     // fallback section
 
-    let pn_glGetString = std::ffi::CString::new("glGetString").unwrap();
-    let local_glGetString_ptr = load_proc(pn_glGetString.as_ptr());
+    let local_glGetString_ptr = load_proc("glGetString");
     if local_glGetString_ptr.is_null() {
         return Err(AgletError::ProcLoad);
     }
-    let local_glGetString: fn(GLenum) -> *const GLubyte = std::mem::transmute(local_glGetString_ptr);
+    let local_glGetString: fn(GLenum) -> *const GLubyte = unsafe {
+        std::mem::transmute(local_glGetString_ptr)
+    };
 
-    let exts_c_str: *const std::ffi::c_uchar = local_glGetString(GL_EXTENSIONS);
-    let exts_str: std::string::String = std::ffi::CStr::from_ptr(exts_c_str.cast()).to_string_lossy().to_string();
-    let gl_err = local_glGetError();
+    let exts_str: std::string::String = unsafe {
+        let exts_c_str: *const std::ffi::c_uchar = local_glGetString(GL_EXTENSIONS);
+        std::ffi::CStr::from_ptr(exts_c_str.cast()).to_string_lossy().to_string()
+    };
+    let gl_err = unsafe { local_glGetError() };
     if gl_err != GL_NO_ERROR {
         return Err(AgletError::GlError);
     }
@@ -300,7 +318,7 @@ unsafe fn load_extensions(load_proc: AgletLoadProc) -> Result<(), AgletError> {
 
         #= foreach extensions =#
         if ext_trimmed == "@{name}" {
-            AGLET_@{name} = true;
+            unsafe { AGLET_@{name} = true; }
             continue;
         }
         #= /foreach =#
@@ -310,8 +328,8 @@ unsafe fn load_extensions(load_proc: AgletLoadProc) -> Result<(), AgletError> {
 }
 
 #[allow(dead_code, non_snake_case)]
-unsafe fn check_minimum_version() -> Result<(), AgletError> {
-    if !AGLET_@{min_api_version} {
+fn check_minimum_version() -> Result<(), AgletError> {
+    if !unsafe { AGLET_@{min_api_version} } {
         eprintln!("[Aglet] Current environment does not support minimum @{api_name} version");
         return Err(AgletError::MinVersion);
     }
@@ -320,11 +338,11 @@ unsafe fn check_minimum_version() -> Result<(), AgletError> {
 }
 
 #[allow(dead_code, non_snake_case)]
-unsafe fn check_required_extensions() -> Result<(), AgletError> {
+fn check_required_extensions() -> Result<(), AgletError> {
     let mut missing_ext = false;
 
     #= foreach extensions =#
-    if !AGLET_@{name} {
+    if !unsafe { AGLET_@{name} } {
         if @{required} {
             eprintln!("[Aglet] Required extension @{name} is not available");
             missing_ext = true;
@@ -342,11 +360,12 @@ unsafe fn check_required_extensions() -> Result<(), AgletError> {
 }
 
 #[allow(dead_code, non_snake_case)]
-unsafe fn load_procs(load_proc: AgletLoadProc) -> Result<(), AgletError> {
-    #= foreach procs =#
-    let pn_@{name} = std::ffi::CString::new("@{name}").unwrap();
-    aglet_@{name} = Some(std::mem::transmute(load_proc(pn_@{name}.as_ptr())));
-    #= /foreach =#
+fn load_procs(load_proc: AgletLoadProc) -> Result<(), AgletError> {
+    unsafe {
+        #= foreach procs =#
+        aglet_@{name} = Some(std::mem::transmute(load_proc("@{name}")));
+        #= /foreach =#
+    }
 
     return Ok(());
 }
@@ -384,15 +403,13 @@ pub(crate) fn agletLoadCapabilities(load_proc: AgletLoadProc) -> Result<(), Agle
 
 #[allow(dead_code, non_snake_case)]
 pub(crate) fn agletLoad(load_proc: AgletLoadProc) -> Result<(), AgletError> {
-    unsafe {
-        if let Err(e) = agletLoadCapabilities(load_proc) {
-            return Err(e);
-        }
-
-        if let Err(e) = load_procs(load_proc) {
-            return Err(e);
-        }
-
-        return Ok(());
+    if let Err(e) = agletLoadCapabilities(load_proc) {
+        return Err(e);
     }
+
+    if let Err(e) = load_procs(load_proc) {
+        return Err(e);
+    }
+
+    return Ok(());
 }
